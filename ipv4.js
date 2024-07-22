218dab48a6f739b887684bdb406736b068772c3b
UniversalBit <31393557+universalbit-dev@users.noreply.github.com>
2024-07-23 00:47:36 +0200

    Delete ipv4.js

--- ipv4.js
+++
@@ -1,9 +0,0 @@
-var exec = require('child_process').exec
-exec('echo $(curl -s https://api.ipify.org)',
-    function (error, stdout, stderr) {
-        ip=console.log('\x1b[33m%s\x1b[0m','IPv4:' + stdout);
-        console.log('stderr: ' + stderr);
-        if (error !== null) {
-             console.log('exec error: ' + error);
-        }
-    });
